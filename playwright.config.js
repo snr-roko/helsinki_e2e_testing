01e69b54c1142b91b77358e688fec9c94f2edf47
mr-roko <rbbagyei@gmail.com>
2024-10-29 13:17:10 +0000

    5.18: Blog List End To End Testing, step 2: Tests written for successful and failed logins. Database for testing is wiped before testing is done in beforeEach function.

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,6 @@ module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: false,
-  timeout: 3000,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,7 +1,18 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const exp = require('constants')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/reset-tests/')
+
+    const newUser = {
+        data: {
+            name: 'David Malan',
+            username: 'mr-david',
+            password: 'secretPassword'
+        }
+    }
+    await request.post('http://localhost:3003/api/users/', newUser)
     await page.goto('http://localhost:5173')
   })
 
@@ -9,10 +20,57 @@ describe('Blog app', () => {
     const blogTitleElement = page.getByText('Blog')
     await expect(blogTitleElement).toBeVisible()
 
-    const usernameButton = page.getByRole('textbox', {name: 'username'})
-    const passwordButton = page.getByRole('textbox', {name: 'password'})
+    const usernameInput = page.getByRole('textbox', {name: 'username'})
+    const passwordInput = page.getByRole('textbox', {name: 'password'})
 
-    await expect(usernameButton).toBeVisible()
-    await expect(passwordButton).toBeVisible()
+    await expect(usernameInput).toBeVisible()
+    await expect(passwordInput).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      const usernameInput = page.getByRole('textbox', {name: 'username'})
+      const passwordInput = page.getByRole('textbox', {name: 'password'})
+
+      await usernameInput.fill('mr-david')
+      await passwordInput.fill('secretPassword')
+
+      const loginButton = page.getByRole('button')
+      await loginButton.click()
+
+      const newBlogButton = page.locator('.new-blog')
+      await expect(newBlogButton).toContainText('Click to add a new blog')
+      await expect(newBlogButton).toBeVisible()
+
+      const user = page.getByText('David Malan logged in')
+      await expect(user).toBeVisible()
+
+      await expect(usernameInput).not.toBeVisible()
+      await expect(passwordInput).not.toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+        const usernameInput = page.getByRole('textbox', {name: 'username'})
+        const passwordInput = page.getByRole('textbox', {name: 'password'})
+  
+        await usernameInput.fill('mr-david')
+        await passwordInput.fill('wrongSecretPassword')
+  
+        const loginButton = page.getByRole('button')
+        await loginButton.click()
+  
+        const newBlogButton = page.getByText('Click to add a new blog')
+        await expect(newBlogButton).not.toBeVisible()
+
+        const user = page.getByText('David Malan logged in')
+        await expect(user).not.toBeVisible()
+
+        const errorMessage = page.getByText('Username or Password is incorrect')
+        await expect(errorMessage).toBeVisible()
+
+        await expect(usernameInput).toBeVisible()
+        await expect(passwordInput).toBeVisible()    
+    })
   })
 })
+
